e04f25b302861ceb0edc5687fdee90da9d378961
Claire Shu <claireshu@fb.com>
2016-06-21 13:58:17 -0700

    SearchView and share link added

--- app/src/main/java/com/claireshu/nysearchtimes_/ArticleActivity.java
+++ app/src/main/java/com/claireshu/nysearchtimes_/ArticleActivity.java
@@ -1,12 +1,18 @@
 package com.claireshu.nysearchtimes_;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class ArticleActivity extends AppCompatActivity {
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,4 +35,23 @@ public class ArticleActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_share, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        // get reference to WebView
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        // pass in the URL currently being used by the WebView
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShareAction.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
--- app/src/main/java/com/claireshu/nysearchtimes_/activities/SearchActivity.java
+++ app/src/main/java/com/claireshu/nysearchtimes_/activities/SearchActivity.java
@@ -2,16 +2,17 @@ package com.claireshu.nysearchtimes_.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.claireshu.nysearchtimes_.Article;
 import com.claireshu.nysearchtimes_.ArticleActivity;
@@ -30,13 +31,12 @@ import java.util.ArrayList;
 import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
-    EditText etQuery;
-    Button btnSearch;
-
     ArrayList<Article> articles;
     ArticleArrayAdapter adapter;
     RecyclerView rvArticles;
     StaggeredGridLayoutManager gridLayoutManager;
+
+    int page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,25 +45,46 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         setUpViews();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+                search(query);
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
+        // handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        //no inspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -72,8 +93,6 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void setUpViews() {
-        etQuery = (EditText) findViewById(R.id.etQuery);
-        btnSearch = (Button) findViewById(R.id.btnSearch);
 
         rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
         articles = new ArrayList<>();
@@ -82,15 +101,6 @@ public class SearchActivity extends AppCompatActivity {
 
         gridLayoutManager = new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL);
         rvArticles.setLayoutManager(gridLayoutManager);
-
-        rvArticles.addOnScrollListener(new EndlessRecyclerViewScrollListener(gridLayoutManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount) {
-                // Triggered only when new data needs to be appended to the list
-                // Add whatever code is needed to append new items to the bottom of the list
-                refresh(page);
-            }
-        });
 
         ItemClickSupport.addTo(rvArticles).setOnItemClickListener(
                 new ItemClickSupport.OnItemClickListener() {
@@ -110,23 +120,22 @@ public class SearchActivity extends AppCompatActivity {
 
 
     }
-    public void onArticleSearch(View view) {
+
+    private void search(final String query) {
         rvArticles.addOnScrollListener(new EndlessRecyclerViewScrollListener(gridLayoutManager) {
             @Override
             public void onLoadMore(int page, int totalItemsCount) {
                 // Triggered only when new data needs to be appended to the list
                 // Add whatever code is needed to append new items to the bottom of the list
-                refresh(page);
+                refresh(page, query);
             }
         });
 
-        refresh(0);
+        refresh(0, query);
     }
-
-    private void refresh(int num) {
-        final int page = num;
-        Log.d("PAGENUM", Integer.toString(page)); 
-        String query = etQuery.getText().toString();
+    private void refresh(int num, String query) {
+        page = num;
+        Log.d("PAGENUM", Integer.toString(page));
         AsyncHttpClient client = new AsyncHttpClient();
         String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
@@ -155,10 +164,8 @@ public class SearchActivity extends AppCompatActivity {
 
                     articles.addAll(newArticles);
 
-                    //adapter.notifyItemRangeInserted(curSize, newArticles.size() - 1);
                     adapter.notifyItemRangeInserted(curSize, articles.size() - 1);
-//                  adapter.notifyDataSetChanged();
-//                  adapter.addAll(Article.fromJSONArray(articleJsonResults));
+
                     Log.d("DEBUG", articles.toString());
                 } catch (JSONException e){
 
